5e1b4c05e4db05e5e51473c179bd09309fa65e2b
king-Alex-d-great <ogubuikealex@gmail.com>
2023-09-28 06:54:06 +0100

    Chore: Update project requirements

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, render_template
-from flask_restful import reqparse, abort, Api, Resource
+from flask_restful import reqparse
 import os
 import pinecone
 from langchain.llms import Replicate
